f71cb06160871a4d0e941ac4877ccadaa228f447
twinklesoni123 <sonitwinkle145@gmail.com>
2026-07-03 22:08:45 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void printFactorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + n + " is: " + factorial);
+    }
+}

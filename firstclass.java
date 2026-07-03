781f46ecba19403f7240ed5b18eee356bcd99f1a
twinklesoni123 <sonitwinkle145@gmail.com>
2026-07-03 22:09:33 +0530

    Add files via upload

---
+++ firstclass.java
@@ -0,0 +1,24 @@
+// class firstclass {
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("* *");
+//         System.out.println("* * *");
+//         System.out.println("* * * *");
+//         System.out.println("* * * * *");
+//         System.out.println("* * * * * *");
+//     }
+// }
+
+
+// public class firstclass {
+//     public static void main(String[] args) {
+//       int n = 4;
+//         for (int i = 1; i <= n; i++) {
+//                 for (int j = 1; j <= i; j++) {
+//                     System.out.print("* ");
+//                 }
+//                 System.out.println();
+//             }
+//     }
+// }
+

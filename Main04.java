f3f9fe2d41f92287b0e1a8c302805243ceee518b
twinklesoni123 <sonitwinkle145@gmail.com>
2026-07-03 22:11:00 +0530

    Add files via upload

---
+++ Main04.java
@@ -0,0 +1,17 @@
+public class Main04 {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 100; i++) {
+        //     System.out.println("* * * * * *");
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();   
+        }
+    }
+       
+}        
+    
+  
